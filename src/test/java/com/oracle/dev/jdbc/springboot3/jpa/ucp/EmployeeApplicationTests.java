fb9355165e8584193a76a5401d3b67cb5291e25d
juarezjuniorgithub <juarez.barbosa@oracle.com>
2023-11-13 15:07:28 +0000

    @juarezjuniorgithub

---
+++ src/test/java/com/oracle/dev/jdbc/springboot3/jpa/ucp/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.dev.jdbc.springboot3.jpa.ucp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
